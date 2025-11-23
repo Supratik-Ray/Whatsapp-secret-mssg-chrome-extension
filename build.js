dbb8fb7fe8987c3847da62654caecddc3bea19ca
Supratik Ray <raySupratik8@gmail.com>
2025-11-23 14:38:13 +0530

    added functionality to decode mssg

---
+++ build.js
@@ -0,0 +1,28 @@
+const esbuild = require("esbuild");
+
+const options = {
+  entryPoints: ["src/content.ts", "src/popup.ts"],
+  outdir: "dist",
+  bundle: true,
+  format: "iife",
+  target: ["chrome110"],
+  sourcemap: false,
+  minify: false,
+};
+
+async function build() {
+  // Watch mode
+  if (process.argv.includes("--watch")) {
+    const ctx = await esbuild.context(options);
+    await ctx.watch();
+    console.log("Watching for changes...");
+  }
+
+  // Normal build
+  else {
+    await esbuild.build(options);
+    console.log("Build completed!");
+  }
+}
+
+build();
---
+++ src/content.ts
@@ -0,0 +1,147 @@
+//mapping + initialization
+const mapping: Record<string, string> = {
+  a: "x9#",
+  b: "p@7",
+  c: "m$4",
+  d: "q!2",
+  e: "z&8",
+  f: "t*5",
+  g: "r^1",
+  h: "k%6",
+  i: "u~3",
+  j: "w+0",
+  k: "b=9",
+  l: "n>4",
+  m: "s<8",
+  n: "c?2",
+  o: "v/7",
+  p: "y|5",
+  q: "j]1",
+  r: "d[6",
+  s: "f}3",
+  t: "g{0",
+  u: "h)9",
+  v: "o(5",
+  w: "i_2",
+  x: "e-7",
+  y: "l;4",
+  z: "a:6",
+  " ": "_0_",
+};
+
+const reverseMapping: Record<string, string> = {};
+
+Object.entries(mapping).forEach(
+  ([key, value]) => (reverseMapping[value] = key)
+);
+
+function waitForElement(element: string): Promise<void> {
+  return new Promise((resolve) => {
+    if (document.querySelector(element)) {
+      resolve();
+      return;
+    }
+    const body = document.querySelector("body");
+    const observer = new MutationObserver((mutations) => {
+      const selectedElement = document.querySelector(element);
+      if (selectedElement) {
+        observer.disconnect();
+        resolve();
+      }
+    });
+
+    observer.observe(body as HTMLBodyElement, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+function decodeText(msg: string) {
+  if (msg.length % 3 !== 0) return "";
+
+  let decoded = "";
+  let startIndex = 0;
+  let endIndex = 2;
+
+  while (endIndex < msg.length) {
+    const substr = msg.slice(startIndex, endIndex + 1);
+    const decodedPart = reverseMapping[substr];
+    if (!decodedPart) return "";
+    decoded += decodedPart;
+    startIndex += 3;
+    endIndex += 3;
+  }
+
+  return decoded;
+}
+
+function InsertDecodedText(decodedText: string, wrapper: HTMLElement) {
+  const divEl = document.createElement("div");
+
+  divEl.style.color = "white";
+  divEl.style.fontSize = "14px";
+  divEl.style.padding = "8px 12px";
+  divEl.style.width = "fit-content";
+  divEl.style.marginTop = "4px";
+  divEl.style.borderRadius = "12px";
+  divEl.style.boxShadow = "0 2px 4px rgba(0,0,0,0.2)";
+  divEl.style.fontFamily = "sans-serif";
+  divEl.style.maxWidth = "80%";
+  divEl.append(decodedText);
+  const isSent = wrapper.classList.contains("message-out");
+  if (isSent) {
+    divEl.style.background = "linear-gradient(135deg, #4CAF50, #2E7D32)";
+  } else {
+    divEl.style.background = "linear-gradient(135deg, #ffb187ff, #a46f14ff)";
+  }
+  wrapper.appendChild(divEl);
+}
+
+function ListenToIncomingMessages() {
+  const chatBodyEl = document.querySelector(
+    'div[data-scrolltracepolicy="wa.web.conversation.messages"]'
+  );
+
+  //watch for new messages
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (node instanceof HTMLElement) {
+          const spanElements = node.querySelectorAll(
+            "span.selectable-text.copyable-text"
+          );
+
+          //decode the messages
+          spanElements.forEach((span) => {
+            const text = span.textContent;
+            const decoded = decodeText(text);
+            if (!decoded) return;
+
+            const msgWrapper = span.closest(".message-in,.message-out");
+            if (!msgWrapper) return;
+
+            //insert the decoded message below the new messages
+            InsertDecodedText(decoded, msgWrapper as HTMLElement);
+          });
+        }
+      }
+    }
+  });
+
+  observer.observe(chatBodyEl as HTMLElement, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+(async function () {
+  await waitForElement(
+    'div[data-scrolltracepolicy="wa.web.conversation.messages"]'
+  );
+  ListenToIncomingMessages();
+})();
+
+//watch for user typing
+//encode it
+//replace with the user's text
